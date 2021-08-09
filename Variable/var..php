69b642b0437c4f61b55dae1d3dbd671546099c81
viveshstrix <vivesh.strixdevelopment@gmail.com>
2021-08-09 16:13:26 -0400

    variables

---
+++ Variable/var..php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>
+            Script
+        </title>
+    </head>
+    <body>
+        <?php
+        $color="black";
+        echo "my car is " . $color . "<br>";
+        ?>
+    </body>
+</html>
