c3a54d292e676da29d73d831f0fb0070261c960e
viveshstrix <vivesh.strixdevelopment@gmail.com>
2021-08-16 12:45:41 +0530

    design

---
+++ Design/bottom.php
@@ -0,0 +1,77 @@
+<section class="top">
+<div class="container">
+    <div class="row">
+        <h4>internal design</h4>
+    </div>
+
+</section>
+<section id="banner">
+<div class="container">
+    <div class="row">
+        <h4>Are you looking for a professional, highly trained and experienced interior designer to help you translate your idea into reality? </h4>
+        <h6>Our expert interior designer is proficient at striking the right balance between science and art. She provides you with an opportunity to take advantage of her education, training and experience to transform your dream property into your reality. </h6>
+    </div>
+</div>
+</section>
+<section id="text">
+    <div class="container">
+        <div class="row">
+            <div class="cont">
+                <div class="leftblock">
+                    <div class="box">
+                        <h3>Add Calmness, Freshness and
+                        <br>
+                        <span class="span">Natural Order to Your Space</span></h3>
+                        <p>As a city or urban dweller, it’s difficult to maintain a sense of belonging with our most innate connection in life, Nature! Recent scientific studies show our physiological response to mild stress is significantly reduced when we’re exposed to literal and or replications of Nature. This knowledge is reflected in the primary Global Trend influencing Interior Design emphasising our ‘appreciation’ for Nature. Our Interior Designer acknowledges well researched and scientifically determined facts of experiencing Nature has a positive effect on our physiological and mental well being.</p>
+                        <p> With our expert interior design services, we help you achieve this health benefit while keeping in view your style preferences, existing trends  and your budget.</p>
+                  </div>
+                        <div class="image"><img src="Images/sofa.jpg" alt="sofa with light decoration">
+                       </div>
+              </div>
+              <div class="cont">
+                      <div class="rightblock">
+                          <div class="image1"><img src="Images/appaten3.jpg"></div>
+                    
+                      <div class="box1">
+                          <h3>Masters of Every Kind <span class="span1"> of Interior Design</span></h3><br>
+                          <p>Whether you wish for an eclectic style, a resort feel interior, a contemporary style, a minimalist look, a Hamptons design, a Scandinavian look, a coastal theme or wish to create a trendy boho atmosphere or if you are simply after a classic or traditional interior, we are masters of every style and design. We have the best interior designing team with years of experience. </p>
+                          <p> With our extensive and diverse expertise in residential interior design, we have the capability to transform any ordinary living space into a piece of creative art. </p>
+                      </div>
+                      </div>
+              </div>
+            </div>
+        </div>
+</section>
+<section id="text1">
+    <div class="container">
+        <div class="row">
+            <div class="leftbl">
+              <div class="block">
+                  <div class="box2">
+                      <h3>Customized Plan Giving Life <br><span class="span1">to Your Dream Interior</span></h3>
+                      <p>After having a comprehensive discussion with you regarding your interior design ideas, preferences and scope of works we create a customized plan. 
+                      Our qualified and professional interior designer discusses the design concept and gives recommendations to achieve the perfect design as per your requirements. We share initial design plans with you before starting work on it. This allows us to make any changes if required prior to executing the interior design plan.</p>
+                    </div>
+             </div>
+             <div class="rgtbl">
+             <div class="image2"><img src="Images/appatent2.jpg">
+             </div>
+      </div>
+    </div>
+</section>
+<section id="text2">
+    <div class="container">
+        <div class="row">
+            <div class="center">
+             <div class="txt">
+                 <h2><span class="span2">Get in touch with us</span><br>
+              to make an appointment to begin your interior design journey!</h2>
+              <a href="contact.php"><button class=bt>CONTACT US</button></a>
+             </div>
+            </div>
+        </div>
+    </div>
+</section>
+<?php
+include_once "footer.php"
+?>
---
+++ Design/header.php
@@ -0,0 +1,40 @@
+ <!DOCTYPE html>
+ <html>
+     <head>
+         <title>
+             Design
+         </title>
+         <link rel="stylesheet" href="Style/style.css">
+     </head>
+     <body>
+         <header>
+         <div class="header">
+             <div class="container">
+                 <div class="row">
+                     <div class="left">
+                         <h3>Design Curtains</h3>
+                     </div>
+                     <div class="right">
+                         <div class="menu">
+                              <ul>
+                                  <li><a href="#">home</a></li>
+                                  <li><a href="#">about us</a></li>
+                                  <li><a href="#">our services</a></li>
+                                  <li><a href="contact.php">contact</a></li>
+                              </ul>
+                         </div>
+                         <div class="media">
+                             <a href="#"><img src="Images/instagram.jpg"></a>
+                             <a href="#"><img src="Images/facebook.png"></a>
+                             <a href="#"><img src="Images/pinterest.png"></a>
+                         </div>
+                     </div>
+                 </div>
+             </div>
+         </div>
+        </header>
+        <?php
+        include_once "bottom.php"
+        ?>
+     </body>
+ </html>
