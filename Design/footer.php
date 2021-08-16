67f7009ae5c60a287fb69c2eaa74077b7d16462a
viveshstrix <vivesh.strixdevelopment@gmail.com>
2021-08-16 10:04:26 +0530

    design

---
+++ Design/footer.php
@@ -0,0 +1,62 @@
+<section id="footer">
+    <div class="container">
+        <div class="row1">
+            <div class="heading">
+                <h3>Design Curtains</h3>
+            </div>
+        </div>
+        <div class="row">
+            <div class="box3">
+                <div class="implinks">
+                    <h5>Important links</h5>
+                </div>
+                <ul>
+                    <li><a href="#">HOME</a></li>
+                    <li><a href="#">ABOUT US</a></li>
+                    <li><a href="#">OUR SERVICES</a></li>
+                    <li><a href="contact.php">CONTACT</a></li>
+                </ul>
+            </div>
+            <div class="box4">
+                <div class="implinks">
+                    <h5>contact information</h5>
+                </div>
+                <div class="continfo">
+                    <p>Castle Hill Homemaker Centre, 9/6 Victoria Ave, Castle Hill NSW 2154, Australia</p>
+                    <p>1300 760 720</p>
+                    <p>(02) 9894 0400</p>
+                    <p>info@designcurtains.com.au</p>
+                
+                <div class="media1">
+                <a href="#" target="_blank"> <img src="https://www.designcurtains.com.au/image/instagram.png"></a>
+                <a href="#" target="_blank"> <img src="https://www.designcurtains.com.au/image/facebook.png"></a>
+				<a href=" #" target="_blank"> <img src="https://www.designcurtains.com.au/image/pinterest.png"></a>
+                </div>
+                </div>
+            </div>
+            <div class="box5">
+                <div class="implinks">
+                    <h5>Contact Form</h5>
+                </div>
+                <div class="form">
+                <form action="#" method="POST" name="form">
+				<input type="text"  placeholder="First Name*" name="fname" required>
+				<input type="text"  placeholder="Last Name*" name="lname" required>
+				<input type="text"  placeholder="Telephone*" name="phone" required>
+				<input type="email"  placeholder="Email*" name="email" required>
+				<textarea placeholder="Enquiry*" name="message"></textarea>
+                <button type="submit" class="button" name="submit">Submit</button>
+                </div>
+            </div>
+        </div>
+</div>
+</section>
+<section id="copyright">
+    <div class="container-copy">
+        <div class="row">
+            <div class="copy">
+                <p>Copyright © 2019  Design Curtains. All Rights Reserved.</p>
+            </div>
+        </div>
+    </div>
+</section>
---
+++ Design/header.php
@@ -0,0 +1,40 @@
+ <!DOCTYPE html>
+ <html>
+     <head>
+         <title>
+             Design
+         </title>
+         <link rel="stylesheet" href="style.css">
+     </head>
+     <body>
+         <header>
+         <div class="header">
+             <div class="container">
+                 <div class="row">
+                     <div class="left">
+                         <h3>Design Curtains</h3>
+                     </div>
+                     <div class="right">
+                         <div class="menu">
+                              <ul>
+                                  <li><a href="#">home</a></li>
+                                  <li><a href="#">about us</a></li>
+                                  <li><a href="#">our services</a></li>
+                                  <li><a href="contact.php">contact</a></li>
+                              </ul>
+                         </div>
+                         <div class="media">
+                             <a href="#"><img src="https://www.designcurtains.com.au/image/instagram.png"></a>
+                             <a href="#"><img src="https://www.designcurtains.com.au/image/facebook.png"></a>
+                             <a href="#"><img src="https://www.designcurtains.com.au/image/pinterest.png"></a>
+                         </div>
+                     </div>
+                 </div>
+             </div>
+         </div>
+        </header>
+        <?php
+        include_once "bottom.php"
+        ?>
+     </body>
+ </html>
