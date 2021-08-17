43624968551a984f9416f6f4af87b5234ff730d6
viveshstrix <vivesh.strixdevelopment@gmail.com>
2021-08-17 13:57:52 +0530

    design

--- Design/contact.php
+++ Design/contact.php
@@ -6,51 +6,53 @@
             Contact
         </title>
     </head>
-<section id="contban">
-<div class="container">
-    <div class="row">
-        <div class="heading1">
-            <h3>CONTACT US</h3>
-        </div>
-    </div>
-</div>
-</section>
-<section id="contban1">
-    <div class="container">
-        <div class="row">
-            <h4>To discuss your interior design project with our friendly team, get in touch with us</h4>
-            <h6>Our custom services team is available to answer your queries and give you a free measure and quote for your project. Please fill in the contact form below and submit. We will get in touch with you within 24 hours to discuss your project needs, ideas and vision.</h6>
-        </div>
-    </div>
-</section>
-<section id="form">
-    <div class="container">
-        <div class="row">
-            <div class="rowtxt">
-                <section id="email">
-                    <div class="container">
-                        <div class="row">
-                            <div class="rowtxt1">
-                                <h4>Contact us through </h4>
-                                <p> info@designcurtains.com.au </p>
-                                <p>1300 760 720  </p>
-                                <h4>reach out to us on phone or just simply fill in the given contact form.</h4>
-                            </div>
-                        </div>
-                    </div>
-                </section>
-            </div>
-        </div>
-    </div>
-    <div class="form1">
-      <form action="#" method="POST" name="form1">
-	      <input type="text"  placeholder="First Name*" name="fname" required><br>
-	      <input type="text"  placeholder="Last Name*" name="lname" required><br>
-	      <input type="text"  placeholder="Telephone*" name="phone" required><br>
-	      <input type="email"  placeholder="Email*" name="email" required><br>
-	      <input type="Enquiry" placeholder="Enquiry*" name="message"><br><br>
-          <button type="submit" class="button" name="submit">Submit</button>
-       </form>
-  </div>
-</section>
+ <body>
+     <section id="contban">
+     <div class="container">
+         <div class="row">
+             <div class="heading1">
+                 <h3>CONTACT US</h3>
+             </div>
+         </div>
+     </div>
+     </section>
+     <section id="contban1">
+         <div class="container">
+             <div class="row">
+                 <h4>To discuss your interior design project with our friendly team, get in touch with us</h4>
+                 <h6>Our custom services team is available to answer your queries and give you a free measure and quote for your project. Please fill in the contact form below and submit. We will get in touch with you within 24 hours to discuss your project needs, ideas and vision.</h6>
+             </div>
+         </div>
+     </section>
+     <section id="form">
+         <div class="container">
+             <div class="row">
+                 <div class="rowtxt">
+                     <section id="email">
+                         <div class="container">
+                             <div class="row">
+                                 <div class="rowtxt1">
+                                     <h4>Contact us through </h4>
+                                     <p> info@designcurtains.com.au </p>
+                                     <p>1300 760 720  </p>
+                                     <h4>reach out to us on phone or just simply fill in the given contact form.</h4>
+                                 </div>
+                             </div>
+                         </div>
+                     </section>
+                 </div>
+             </div>
+         </div>
+         <div class="form1">
+           <form action="#" method="POST" name="form1">
+     	      <input type="text"  placeholder="First Name*" name="fname" required><br>
+     	      <input type="text"  placeholder="Last Name*" name="lname" required><br>
+     	      <input type="text"  placeholder="Telephone*" name="phone" required><br>
+     	      <input type="email"  placeholder="Email*" name="email" required><br>
+     	      <input type="Enquiry" placeholder="Enquiry*" name="message"><br><br>
+               <button type="submit" class="button" name="submit">Submit</button>
+            </form>
+       </div>
+      </section>
+  </body>
 </html>
