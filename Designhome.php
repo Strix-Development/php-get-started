a0eecd99379562b9b0c4be103a0e3633437c8ee7
viveshstrix <vivesh.strixdevelopment@gmail.com>
2021-08-19 11:33:30 +0530

    design

---
+++ Designhome.php
@@ -0,0 +1,2 @@
+<link rel="stylesheet" href="Style/style.css">
+<?php include_once __DIR__."/indexx.php" ?>
---
+++ indexx.php
@@ -0,0 +1,5 @@
+<?php
+include_once __DIR__. "/Designheader.php";
+include_once __DIR__. "/Designbottom.php.";
+include_once __DIR__. "/Designfooter.php";
+?>
