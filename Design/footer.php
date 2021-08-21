0e7d57dfebe9e3881a2440b2614170f148e5f5c1
viveshstrix <vivesh.strixdevelopment@gmail.com>
2021-08-21 20:42:55 +0530

    design

---
+++ Design/footer.php
@@ -0,0 +1,58 @@
+<section id="footer">
+     <div class="contt">
+         <div class="boxx">
+             <div class="headingg">
+                  <h4>design curtains</h4>
+             </div>
+             <div class="boxes">
+                 <div class="boxno1">
+                     <div class="linkss">
+                         <h6>important links</h6>
+                     </div>
+                     <div class="content">
+                          <ul>
+                             <li><a href="index.php">HOME</a></li>
+                             <li><a href="#">ABOUT</a></li>
+                             <li><a href="#">OUR SERVICES</a></li>
+                             <li><a href="contact.php">CONTACT</a></li>
+                          </ul>
+                      </div>
+                 </div>
+                 <div class="boxno2">
+                     <div class="linksss">
+                         <h6>contact information</h6>
+                     </div>
+                     <div class="continfo">
+                         <p>Castle Hill Homemaker Centre, 9/6 Victoria Ave, Castle Hill NSW 2154, Australia</p>
+                         <p>1300 760 720</p>
+                         <p>(02) 9894 0400</p>
+                         <p>info@designcurtains.com.au</p>
+                     </div>
+                     <div class="media1">
+                         <a href="#"><img src="Images/instagram.jpg"></a>
+                         <a href="#"><img src="Images/facebook.png"></a>
+                         <a href="#"><img src="Images/pinterest.png"></a>
+                     </div>
+                 </div>
+                 <div class="boxno3">
+                       <div class="linkssss">
+                         <h6>contact form</h6>
+                       </div>
+                      <div class="form">
+                         <form action="#" method="POST" name="form">
+			                 <input type="text"  placeholder="First Name*" name="fname" required><br>
+				             <input type="text"  placeholder="Last Name*" name="lname" required><br>
+				             <input type="text"  placeholder="Telephone*" name="phone" required><br>
+			                 <input type="email"  placeholder="Email*" name="email" required><br>
+				             <input type="Enquiry" placeholder="Enquiry*" name="message"><br>
+                             <button type="submit" class="button" name="submit">Submit</button>
+                          </form>
+                     </div>
+                 </div>
+             </div>
+          <div>
+      </div>
+     <div class="copy">
+         <p>Copyright © 2019 Design Curtains. All Rights Reserved.</p>
+     </div>  
+</section>  
