d5fd1fd8e9c538c9b1bd07d088756add49c0bb0c
viveshstrix <vivesh.strixdevelopment@gmail.com>
2021-08-21 20:37:33 +0530

    forms

---
+++ Forms/form.php
@@ -0,0 +1,7 @@
+<html>
+    <body>
+        Hello <?php echo $_POST["fname"]; ?><br>
+        Your information is saved as :<br>Phone Number : <?php echo $_POST["number"]."<br>";?>
+        E-mail : <?php echo $_POST["email"];?>
+    </body>
+</html>
