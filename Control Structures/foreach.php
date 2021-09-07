1626cf9fdd6986141555044ffd63fe26adf55d61
viveshstrix <vivesh.strixdevelopment@gmail.com>
2021-09-07 15:24:50 +0530

    control

---
+++ Control Structures/foreach.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Foreach</title>
+</head>
+<body>
+   <?php
+   $var=array(1,2,3,4,5,6,7,8,9,10);
+   foreach($var as $val){
+   echo "$val <br>";
+   }
+   ?> 
+</body>
+</html>
