e9c149f6c08966e6f62a0bc0ba246a07af63ee66
viveshstrix <vivesh.strixdevelopment@gmail.com>
2021-09-07 15:24:50 +0530

    control structure

---
+++ Control Structures/index.php
@@ -0,0 +1,7 @@
+<?php
+// require_once __DIR__. "/if_statements.php";
+// require_once __DIR__. "/while.php";
+// require_once __DIR__. "/do_while.php";
+// require_once __DIR__. "/for.php";
+require_once __DIR__. "/foreach.php";
+?>
