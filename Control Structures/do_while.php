e9704935f7c9b5aee28aa13f7d1d1b2c1dd82831
viveshstrix <vivesh.strixdevelopment@gmail.com>
2021-09-07 14:59:18 +0530

    control structure

--- Control Structures/do_while.php
+++ Control Structures/do_while.php
@@ -18,25 +18,6 @@
         echo "<br>";
         $i++;
     }while($i>1);
-    echo "<h1>For</h1>";
-    for($i=0;$i<=10;$i++)
-    echo "$i <br>";
-    echo "<br>";
-    for ($i=0; $i<=100 ; $i+=10) { 
-    echo "$i <br>";
-    }
-    echo "<h1>Tables</h1>";
-    $i=1;
-    do{
-        echo " 9 * $i = ". 9 * $i ."<br>";
-        $i++;
-    }while($i<=10);
-    echo "<br>";
-    $i=1;
-    do{
-        echo " 10 * $i = ". 10 * $i  . "<br>";
-        $i++;
-    }while($i<=10);
     ?>
 </body>
 </html>
