ab11220e389e2e15c95825f54efe82c6c192508b
viveshstrix <vivesh.strixdevelopment@gmail.com>
2021-09-13 15:38:20 +0530

    type

--- Types/index.php
+++ Types/index.php
@@ -1,6 +1,7 @@
 <?php
 require_once __DIR__. "/Array/index.php";
 require_once __DIR__. "/Callback/index.php";
+require_once __DIR__.. "/Iterable/index.php"
 require_once __DIR__. "/Object/index.php";
 require_once __DIR__. "/Types/index.php";
 ?>
