232b6d974d5bdb9313e21f283ee030babe9eb78f
viveshstrix <vivesh.strixdevelopment@gmail.com>
2021-09-13 15:56:07 +0530

    type

--- Types/Object/object.php
+++ Types/Object/object.php
@@ -26,8 +26,7 @@
             function number($ch_no){
             return $this->number=$ch_no;
             }
-        }
-        
+        }        
         $obj= new TV;
         echo'Name: '.$obj->fname('ABC');
         echo"<br>";
@@ -52,8 +51,61 @@
        $object= new Khilone;
        echo 'Name: '.$object->fname('Truck')."<br>";
        echo 'Color: '.$object->fname1('Black')."<br>";
+       echo "<br>";
        echo 'Quantity: '.$object->fname2('2');
+       class Fruit{
+        public $name;
+        function set_name($name){
+            $this->name =$name;
+        }
+        function get_name(){
+            return $this->name;
+        }
+    }
+    $apple = new Fruit();
+    $apple->set_name('Apple');
+    echo $apple->get_name();
+    echo "<br>";
+    class fruits{
+        public $name;
+        public $color;
+        function set_name($set){
+            $this->name=$set;
+        }
+        function get_name(){
+            return $this->name;
+        }
+        function set_color($col){
+            $this-> color=$col;
+        }
+        function get_color(){
+           return $this-> color;
+        }
+    }
+    $apple= new fruits();
+    $apple->set_name('Apple');
+    echo "Name:".$apple->get_name();
+    echo "<br>";
+    $apple->set_name('Red');
+    echo "Color:".$apple->get_name();
+    echo "<br>";
 
+    class foo{
+        function do_foo(){
+            echo "Doing Foo";
+        }
+    }
+    $bar= new foo;
+    $bar->do_foo();
+    echo"<br>";
+
+    class hello{
+        function hi(){
+            echo "Hello World";
+        }
+    }
+    $hii= new hello;
+    $hii->hi()
         ?>
     </body>
 </html>
