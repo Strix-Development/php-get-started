8dd60659bd98a0e474421852934526c3e39a47ae
unknown <ishantstrixdevelopment@gmail.com>
2021-09-13 09:46:26 +0530

    switch

--- switch/switch.php
+++ switch/switch.php
@@ -15,7 +15,43 @@ switch ($favcolor) {
   default:
     echo "Your favorite color is neither red, blue, nor green!";
 }
+echo "<br>";
+
+$today= date("D");
+switch($today){
+  case "Mon":
+  echo "Today is monday.Worst day";
+  break;
+
+  case "Tue":
+  echo "Today is tuesday.Decent day";
+  break;
+
+  case "Wed":
+  echo "Today is wednesday.Excited day";
+  break;
+
+  case "Thu":
+  echo "Today is thursday.Fantastic day";
+  break;
+
+  case "Fri":
+  echo "Today is friday.Party day";
+  break;
 
 
+  case "Sat":
+  echo "Today is saturday.Holiday";
+  break;
 
+  case "Sun":
+  echo "Today is sunday.Best day";
+  break;
+
+  default :
+  echo "Nothing is specified";
+}
+
+echo date("Y");
+// echo time()
 ?>
