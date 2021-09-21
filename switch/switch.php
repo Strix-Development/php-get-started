94f89c0e3905004e6549d831cca61619b30a5966
unknown <ishant.strixdevelopment@gmail.com>
2021-09-21 14:09:55 +0530

    ishan

--- switch/switch.php
+++ switch/switch.php
@@ -56,4 +56,108 @@ echo date("Y");
 // echo time()
 echo "<br>";
 echo "<br>";
+
+?>
+
+<?php
+
+// const hello='ishan sharma';
+// return hello;
+
+function name($name){
+  return $name ."ishan";
+}
+function hello($name, $ishan){
+  echo $ishan($name);
+
+}
+hello("hi","name");
+echo "<br>";
+?>
+
+<?php
+
+$var="hello";
+$$var="world";
+
+echo "$var ${$var}";
+
+echo "<br>";
+$i=11;
+do{
+  echo "hello";
+  $i++;
+  }
+while($i<=10);
+
+echo "<br>";
+
+$var=<<< yes
+ishanjsnj
+aidajsokjlas
+aoisojkdwaslk
+
+yes;
+echo $var;
+$i=0;
+if($i==0){
+  echo 'yo';
+  // break;
+}
+?>
+<br>
+<?php
+$cars="lamborghini";
+switch($cars){
+  case 'maruti';
+  echo "not mytype";
+  break;
+  case 'lamborghini';
+  echo "love😍" ;
+  break;
+  default:
+  echo "lambo or nothing";
+}
+
+$var=`you`;
+echo $var;
+?>
+
+<?php
+$output = `ls -al`;
+echo "<pre>$output</pre>";
+?>
+
+<?php
+
+
+@include 'loops/whil.php';
+
+$var=10;
+$var1=10.5;
+var_dump($var*$var1);
+
+echo `dir`;
+
+echo "<br>";
+
+$var="hello";
+@var_dump($vari);
+?>
+
+<?php
+
+$a=10;
+$b=5;
+var_dump( $a | $b);
+
+$c=null;
+var_dump($c);
+
+const name="hello";
+echo name;
+
+define('i',$a);
+echo i;
+
 ?>
