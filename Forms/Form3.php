54a8a84193575ee747828b08b41d892319b97b65
viveshstrix <vivesh.strixdevelopment@gmail.com>
2021-09-22 10:00:10 +0530

    form

--- Forms/Form3.php
+++ Forms/Form3.php
@@ -2,76 +2,22 @@
 <html>
 <head>
     <title>Form</title>
-    <style>
-      .err{
-        color:red;
-      }
-    </style>
 </head>
 <body>
-  <?php
-    $passerr=$fnameerr=$lnameerr=$emailerr=$passwerr=$numerr=$conferr="";
-     if (isset($_POST["submit"])) {
-      if($_POST["password"]!=$_POST["confirmpassword"]){
-        $passerr="<b><i>Password doesnt match</i></b>";
-      }
-      if(empty($_POST["fname"])){
-        $fnameerr= "<b><i>Please enter your first name</i></b>";
-     }
-     if(empty($_POST["lname"])){
-      $lnameerr= "<b><i>Please enter your last name</i></b>";
-    }
-    if(empty($_POST["email"])){
-      $emailerr= "<b><i>Please enter your email</i></b>";
-    }
-    if(empty($_POST["password"])){
-      $passwerr="<b><i>Please enter your password</i></b>";
-    }
-    if(empty($_POST["number"])){
-      $numerr= "<b><i>Please enter your number</i></b>";
-    }
-    if(empty($_POST["confirmpassword"])){
-      $conferr= "<b><i>Please confirm your password</i></b>";
-    }
-   }
-  ?>
-  <form action="" method="POST">
+  <form action="register.php" method="POST">
    <label for="fname">First Name</label><br>
-   <input type="text" name="fname" id="fname">
-   <span class="err"> <?php echo $fnameerr ?></span><br>
+   <input type="text" name="fname" id="fname"><br>
    <label for="lname">Last Name</label><br>
-   <input type="text" name="lname" id="lname">
-   <span class="err"><?php echo $lnameerr ?></span><br>
+   <input type="text" name="lname" id="lname"><br>
    <label for="email">E-mail</label><br>
-   <input type="email" name="email" id="email">
-   <span class="err"><?php echo $emailerr ?></span><br>
+   <input type="email" name="email" id="email"><br>
    <label for="password">Password</label><br>
-   <input type="password" name="password" id="password">
-   <span class="err"><?php echo $passwerr ?><?php echo $passerr ?></span><br>
+   <input type="password" name="password" id="password"><br>
    <label for="confirmpassword">Confirm Password</label><br>
-   <input type="password" name="confirmpassword" id="confirmpassword">
-   <span class="err"><?php echo $conferr ?><?php echo $passerr ?></span><br>
+   <input type="password" name="confirmpassword" id="confirmpassword"><br>
    <label for="number">Phone Number</label><br>
-   <input type="tel" name="number" id="number" pattern="[0-9]{10}">
-   <span class="err"><?php echo $numerr ?></span><br><br>
+   <input type="tel" name="number" id="number" pattern="[0-9]{10}" maxlength="10"><br><br>
    <input type="submit" name="submit" value="Submit">
   </form>
-  <?php
-   if (isset($_POST["submit"])) {
-     echo "<h3>Your information:</h3>";
-      if($_POST["fname"]){
-         echo "<b>Full Name:</b>".$_POST["fname"]."&nbsp".$_POST["lname"];
-         echo "<br>";
-     }
-     if($_POST["email"]){
-      echo "<b>E-mail:</b>".$_POST["email"];
-      echo "<br>";
-     }  
-     if($_POST["number"]){
-         echo "<b>Number:</b>".$_POST["number"];
-         echo "<br>";
-     }
-    }
- ?>
 </body>
 </html>
--- Forms/register.php
+++ Forms/register.php
@@ -1,28 +1,64 @@
+<style>
+    .error{
+        color:red;
+    }
+</style>
 <?php
-if(empty($_POST["fname"])){
-    echo "Please enter your first name <br>";
-}else{
-    echo "First Name:".$_POST["fname"];
-    echo "<br>";
+// if(isset($_POST["submit"])){
+//   if(empty($_POST["fname"])){
+//       echo "Please enter your first name <br>";
+//   }else{
+//       echo "First Name:".$_POST["fname"];
+//       echo "<br>";
+//   }
+//   if(empty($_POST["lname"])){
+//       echo "Please enter your last name<br>";
+//   }else{
+//       echo "Last Name:".$_POST["lname"];
+//       echo "<br>";
+//   }
+//   if(empty($_POST["email"])){
+//       echo "Please enter your email<br>";
+//   }
+//   if(empty($_POST["password"])){
+//       echo "Please enter your password<br>";
+//   }
+//   if ($_POST["password"]!=$_POST["confirmpassword"]) {
+//       echo "Your password doesn't match <br>";
+//   }
+//   if(empty($_POST["number"])){
+//       echo "Please enter your number<br>";
+//   }else{
+//       echo "Number:".$_POST["number"];
+// }
+if(isset($_POST["submit"])){
+if($_POST["password"]!=$_POST["confirmpassword"]){
+    echo "<span class='error'>Your password doesnt match</span>";
 }
-if(empty($_POST["lname"])){
-    echo "Please enter your last name<br>";
-}else{
-    echo "Last Name:".$_POST["lname"];
-    echo "<br>";
+if($_POST["password"]==$_POST["confirmpassword"]){
+    if(empty($_POST["fname"])){
+        echo"<span class='error'>Please enter your first name<br></span>";
+    }else{
+       echo "<b>First Name:</b>".$_POST["fname"]."<br>";
+    }
+    if(empty($_POST["lname"])){
+        echo"<span class='error'>Please enter your last name<br></span>";
+    }else{
+        echo "<b>Last Name:</b>".$_POST["lname"]."<br>";
+    }
+    if(empty($_POST["email"])){
+        echo"<span class='error'>Please enter your email<br></span>";
+    }else{
+        echo "<b>E-mail:</b>".$_POST["email"]."<br>";
+    }
+    if(empty($_POST["password"])){
+        echo"<span class='error'>Please enter your password<br></span>";
+    }
+    if(empty($_POST["number"])){
+        echo"<span class='error'>Please enter your number<br></span>";
+    }else{
+        echo "<b>Number:</b>".$_POST["number"]."<br>";
+    }
 }
-if(empty($_POST["email"])){
-    echo "Please enter your email<br>";
-}
-if(empty($_POST["password"])){
-    echo "Please enter your password<br>";
-}
-if ($_POST["password"]!=$_POST["confirmpassword"]) {
-    echo "Your password doesn't match <br>";
-}
-if(empty($_POST["number"])){
-    echo "Please enter your number<br>";
-}else{
-    echo "Number:".$_POST["number"];
 }
 ?>
