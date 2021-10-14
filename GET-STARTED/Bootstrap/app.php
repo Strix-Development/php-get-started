a0c9d701f9ab23603adf5210660709f75cf32672
viveshstrix <vivesh.strixdevelopment@gmail.com>
2021-10-14 16:45:28 +0530

    get-started

--- GET-STARTED/Bootstrap/app.php
+++
@@ -1,3 +0,0 @@
-<?php
-require_once __DIR__."/../Core/Support/kernel.php";
-?>
--- GET-STARTED/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once __DIR__ . "/Bootstrap/app.php";
-//header called
-require_once file_header();
-//master called
-require_once file_master();
-//footer called
-require_once file_footer();
-?>
