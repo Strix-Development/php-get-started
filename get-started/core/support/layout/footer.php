84c471d76fec8ad1c380e8cf9e82303be087f5ad
sharmayaman <yaman.strixdevelopment@gmail.com>
2021-10-15 11:13:38 +0530

    project form handling

---
+++ get-started/core/support/layout/footer.php
@@ -0,0 +1,27 @@
+<style>
+    .foot {
+
+        padding: 30px;
+        text-align: center;
+        background: #212529;
+        color: white;
+        height: 80px;
+
+    }
+</style>
+
+<body>
+    <footer>
+        <div class="foot">
+
+            <h5>Copyright &#169; 2021 himachal tourism. All Rights Reserved. &#165;
+                <span style="font-family: fantasy; text-transform: uppercase;">yaman</span>
+            </h5>
+
+        </div>
+
+
+    </footer>
+</body>
+
+</html>
---
+++ get-started/core/support/layout/header.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Himachal Tourism </title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+
+<body>
+    <header>
+        <div class="m-4">
+            <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
+                <div class="container-fluid">
+                    <a href="index.php" class="navbar-brand">HIMACHAL TOURISM</a>
+                    <button type="button" class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div id="navbarCollapse" class="collapse navbar-collapse">
+                        <ul class="nav navbar-nav">
+                            <li class="nav-item">
+                                <a href="#" class="nav-link">Home</a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="#" class="nav-link">Profile</a>
+                            </li>
+                        </ul>
+                        <ul class="nav navbar-nav ms-auto">
+                            <li class="nav-item dropdown">
+                                <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">User ID</a>
+                                <div class="dropdown-menu dropdown-menu-end bg-dark">
+                                    <a href="get-started/login.php" class="dropdown-item" style="color: white;">Log in</a>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    </header>
