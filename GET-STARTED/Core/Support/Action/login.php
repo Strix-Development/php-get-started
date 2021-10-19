43cd95a359796514fdee16d8b839c48151f0cd6a
viveshstrix <vivesh.strixdevelopment@gmail.com>
2021-10-19 14:47:35 +0530

    signup form

--- GET-STARTED/Core/Support/Action/login.php
+++ GET-STARTED/Core/Support/Action/login.php
@@ -1,2 +1,19 @@
 <?php
+if ($_POST['email']==''||$_POST['password']=='') {
+    echo "All fields are required";
+}else{
+$action = json_decode(file_get_contents('Database/user.json'), true);
+$filter = array_filter($action, function ($action) {
+    if ($action['E-mail'] == $_POST['email']) {
+        if ($action['Password'] == $_POST['password']) {
+            return $action;
+        }
+    }
+});
+if (count($filter)) {
+    echo "Welcome";
+} else {
+    echo "Invalid email or password";
+}
+}
 ?>
